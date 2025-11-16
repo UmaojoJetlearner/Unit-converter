73d138fdcd0686d7fa1b72006d1d5f7814c24421
UmaojoJetlearner <umaojo.okpanachi@gmail.com>
2025-11-16 13:56:56 -0600

    Add files via upload

---
+++ Unit Convertor/Main.py
@@ -0,0 +1,31 @@
+# def addition(num1,num2):
+#     result=num1+num2
+#     print(result)
+
+# addition(5,12)
+
+def area(length,width):
+    product=length*width
+    print(product)
+
+area(10,10)
+
+def add(num1,nu2):
+    return num1+nu2
+
+answer=add(23,34)
+print(answer)
+    
+
+
+def divide(inch):
+    return inch/12
+
+quotient=divide(36)
+print(quotient)
+
+def temp(c):
+    return c*1.8+32
+
+weather=temp(23)
+print(weather)
